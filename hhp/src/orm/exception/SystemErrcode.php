6350e18da9e1e411bdc64f5401fef6741f47b53c
hoheart <hoheart@163.com>
2014-12-14 18:33:08 +0800

    orm test passed.

---
+++ hhp/src/orm/exception/SystemErrcode.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace orm\exception;
+
+class SystemErrcode extends \hhp\exception\SystemErrcode {
+}
+?>
---
+++ hhp/src/orm/exception/UserErrcode.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace orm\exception;
+
+class UserErrcode extends \hhp\exception\UserErrcode {
+	const ParseClassDescError = 4200;
+	const NoPropertyError = 4201;
+}
+?>
