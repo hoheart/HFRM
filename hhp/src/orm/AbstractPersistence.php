8d2f467e86a36d7ea98a55d34c1357a991ec2cd9
hoheart <hoheart@163.com>
2014-12-20 17:44:30 +0800

    redesigned the persistent.

--- hhp/src/orm/AbstractPersistence.php
+++ hhp/src/orm/AbstractPersistence.php
@@ -13,19 +13,18 @@ abstract class AbstractPersistence {
 	/**
 	 * 保存数据对象，包括了的添加和更新情况，会自己判断是更新还是添加操作。
 	 *
-	 * @param object $dataObj        	
+	 * @param DataClass $dataObj        	
 	 * @param ClassDesc $clsDesc        	
 	 * @return 如果是新增，dataObj的主键值将被改写，并返回-1；如果是更新，返回影响的行数；如果该对象不需要保存，则反回0。
 	 */
-	public function save ($dataObj, ClassDesc $clsDesc = null) {
+	public function save (DataClass $dataObj, ClassDesc $clsDesc = null) {
 		if (null == $clsDesc) {
 			$clsDesc = DescFactory::Instance()->getDesc(get_class($dataObj));
 		}
 		
 		$status = $this->getPropertyVal($dataObj, 'mDataObjectExistingStatus');
 		// 只要开始保存这个对象，就设置保存成功，否则两个类相互引用会死循环。
-		$this->setPropertyVal($dataObj, 'mDataObjectExistingStatus', 
-				DataClass::DATA_OBJECT_EXISTING_STATUS_SAVED);
+		$this->setPropertyVal($dataObj, 'mDataObjectExistingStatus', DataClass::DATA_OBJECT_EXISTING_STATUS_SAVED);
 		
 		if (DataClass::DATA_OBJECT_EXISTING_STATUS_NEW == $status) {
 			return $this->add($dataObj, $clsDesc);
@@ -36,10 +35,29 @@ abstract class AbstractPersistence {
 		}
 	}
 
-	abstract protected function add ($dataObj, ClassDesc $clsDesc);
+	/**
+	 * 添加操作
+	 *
+	 * @param DataClass $dataObj        	
+	 * @param ClassDesc $clsDesc        	
+	 */
+	abstract protected function add (DataClass $dataObj, ClassDesc $clsDesc);
 
-	abstract protected function update ($dataObj, ClassDesc $clsDesc);
+	/**
+	 * 更新操作
+	 *
+	 * @param DataClass $dataObj        	
+	 * @param ClassDesc $clsDesc        	
+	 */
+	abstract protected function update (DataClass $dataObj, ClassDesc $clsDesc);
 
+	/**
+	 * 通过ReflectionClass设置属性的值。
+	 *
+	 * @param DataClass $dataObj        	
+	 * @param string $attrName        	
+	 * @param mixed $val        	
+	 */
 	protected function setPropertyVal ($dataObj, $attrName, $val) {
 		$refCls = new \ReflectionClass(get_class($dataObj));
 		$refProperty = $refCls->getProperty($attrName);
@@ -47,6 +65,13 @@ abstract class AbstractPersistence {
 		$refProperty->setValue($dataObj, $val);
 	}
 
+	/**
+	 * 通过ReflectionClass取得属性的值。
+	 *
+	 * @param DataClass $dataObj        	
+	 * @param string $attrName        	
+	 * @return mixed
+	 */
 	protected function getPropertyVal ($dataObj, $attrName) {
 		$refCls = new \ReflectionClass(get_class($dataObj));
 		$refProperty = $refCls->getProperty($attrName);
@@ -61,23 +86,5 @@ abstract class AbstractPersistence {
 	 * @param Condition $condition        	
 	 */
 	abstract public function delete ($className, Condition $condition = null);
-
-	/**
-	 * 更新是class属性对应的另一个非class属性的值。
-	 * 用于保存一个对象时，同时对其属性是class的属性也进行了保存，这时，就需要更新另一个非class的属性。
-	 *
-	 * @param DataClass $mainObj
-	 *        	要更行的对象
-	 * @param ClassAttribute $attr
-	 *        	对应的属性是class的ClassAttribute
-	 * @param DataClass $anotherObj
-	 *        	mainObj对应的class属性的值
-	 */
-	protected function updateForeignKey (DataClass $mainObj, ClassAttribute $attr, $anotherObj) {
-		$mainKey = $attr->selfAttribute2Relationship;
-		$anotherKey = $attr->anotherAttribute2Relationship;
-		$anotherVal = $anotherObj->$anotherKey;
-		$mainObj->$mainKey = $anotherVal;
-	}
 }
 ?>
