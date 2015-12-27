2ff1a55bca5d8d7a1a8440785c9c1713fa234c71
Hoheart <youkejiang@che001.com>
2015-12-28 02:27:02 +0800

    remove terminalType

--- Controller.php
+++ Controller.php
@@ -8,19 +8,6 @@ use HFC\Exception\ParameterErrorException;
 abstract class Controller {
 	
 	/**
-	 * 请求终端端的来源
-	 *
-	 * @var integer
-	 */
-	const TERMINAL_TYPE_UNKNOW = 0;
-	const TERMINAL_TYPE_WEB = 1;
-	const TERMINAL_TYPE_ANDROID_PHONE = 2;
-	const TERMINAL_TYPE_IPHONE = 3;
-	const TERMINAL_TYPE_WAP = 4;
-	const TERMINAL_TYPE_ANDROID_PAD = 5;
-	const TERMINAL_TYPE_IPAD = 6;
-	
-	/**
 	 * 模块别名
 	 *
 	 * @var string
@@ -36,8 +23,6 @@ abstract class Controller {
 
 	public function __construct ($alias) {
 		$this->mModuleAlias = $alias;
-		
-		Session::Instance()->set('FRM_terminalType', self::TERMINAL_TYPE_WEB);
 	}
 
 	public function setView ($name, $viewType = View::VIEW_TYPE_HTML) {
