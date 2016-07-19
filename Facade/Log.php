31a352ce45bf1a3cd9f1d05fc52994cb7a065329
Hoheart <youkejiang@che001.com>
2016-07-19 15:25:25 +0800

    修复日志模块传递错误的bug

--- Facade/Log.php
+++ Facade/Log.php
@@ -8,10 +8,10 @@ use Framework\HFC\Log\Logger;
 class Log {
 
 	static public function r ($desc, $module, RequestContext $context = null) {
-		Service::get('log')->log($desc, $module, Logger::LOG_TYPE_RUN, Logger::LOG_LEVEL_FATAL, $context);
+		Service::get('log')->log($desc, Logger::LOG_TYPE_RUN, $module, Logger::LOG_LEVEL_FATAL, $context);
 	}
 
 	static public function e ($desc, $module, $level, RequestContext $context = null) {
-		Service::get('log')->log($desc, $module, Logger::LOG_TYPE_ERROR, $level, $context);
+		Service::get('log')->log($desc, Logger::LOG_TYPE_ERROR, $module, $level, $context);
 	}
 }
