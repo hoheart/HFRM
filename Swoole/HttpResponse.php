ac5aeb35b065c753c2c933ea35d978668bdd6fca
Hoheart <youkejiang@che001.com>
2016-08-01 17:25:52 +0800

    Swoole\HttpResponse增加了addBody方法

--- Swoole/HttpResponse.php
+++ Swoole/HttpResponse.php
@@ -1,54 +1,67 @@
 <?php
-
 namespace Framework\Swoole;
 
 use Framework\Exception\NotImplementedException;
 use Framework\IHttpResponse;
 
-class HttpResponse implements IHttpResponse {
-	
-	/**
-	 *
-	 * @var \swoole_http_response
-	 */
-	protected $mSwooleResponse = null;
-	
-	/**
-	 *
-	 * @var string $mBody
-	 */
-	protected $mBody = '';
+class HttpResponse implements IHttpResponse
+{
 
-	public function __construct ($resp) {
-		$this->mSwooleResponse = $resp;
-	}
+    /**
+     *
+     * @var \swoole_http_response
+     */
+    protected $mSwooleResponse = null;
 
-	public function setStatusCode ($code) {
-		$this->mSwooleResponse->status($code);
-	}
+    /**
+     *
+     * @var string $mBody
+     */
+    protected $mBody = '';
 
-	public function setReasonPhrase ($reason) {
-		throw new NotImplementedException();
-	}
+    public function __construct($resp)
+    {
+        $this->mSwooleResponse = $resp;
+    }
 
-	public function setHeader ($fieldName, $value, $replace = true) {
-		$this->mSwooleResponse->header($fieldName, $value);
-	}
+    public function setStatusCode($code)
+    {
+        $this->mSwooleResponse->status($code);
+    }
 
-	public function setCookie ($key, $value = '', $expire = 0, $path = '/', $domain = '', $secure = false, $httponly = false) {
-		$this->mSwooleResponse->cookie($key, $value, $expire, $path, $domain, $secure, $httponly);
-	}
+    public function setReasonPhrase($reason)
+    {
+        throw new NotImplementedException();
+    }
 
-	/**
-	 * 设置响应内容
-	 *
-	 * @param string $body        	
-	 */
-	public function setBody ($body) {
-		$this->mBody = $body;
-	}
+    public function setHeader($fieldName, $value, $replace = true)
+    {
+        $this->mSwooleResponse->header($fieldName, $value);
+    }
 
-	public function getBody () {
-		return $this->mBody;
-	}
+    public function setCookie($key, $value = '', $expire = 0, $path = '/', $domain = '', $secure = false, $httponly = false)
+    {
+        $this->mSwooleResponse->cookie($key, $value, $expire, $path, $domain, $secure, $httponly);
+    }
+
+    /**
+     * 设置响应内容
+     *
+     * @param string $body
+     *            内容
+     */
+    public function setBody($body)
+    {
+        $this->mBody = $body;
+    }
+
+    public function getBody()
+    {
+        return $this->mBody;
+    }
+
+    public function addBody($str)
+    {
+        throw new NotImplementedException();
+    }
 }
