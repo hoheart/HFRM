f1bb0cac50926c21f6e90dc0a3e46555036d7ef5
Hoheart <youkejiang@che001.com>
2016-08-01 18:45:47 +0800

    增加了对上传文件的支持

--- Swoole/HttpRequest.php
+++ Swoole/HttpRequest.php
@@ -1,132 +1,152 @@
 <?php
-
 namespace Framework\Swoole;
 
 use Framework\IHttpRequest;
 use Framework\Exception\NotImplementedException;
 
-class HttpRequest implements IHttpRequest {
-	
-	/**
-	 *
-	 * @var string
-	 */
-	const COOKIE_NAME_REQUESTID = 'RequestId';
-	
-	/**
-	 *
-	 * @var swoole_http_request
-	 */
-	protected $mRequest = null;
-	
-	/**
-	 *
-	 * @var array $mAllParam
-	 */
-	protected $mAllParam = array();
+class HttpRequest implements IHttpRequest
+{
 
-	public function __construct ($req) {
-		$this->mRequest = $req;
-	}
+    /**
+     *
+     * @var string
+     */
+    const COOKIE_NAME_REQUESTID = 'RequestId';
 
-	public function getId () {
-		$id = $this->getCookie(self::COOKIE_NAME_REQUESTID);
-		if (empty($id)) {
-			$id = uuid_create();
-			
-			$this->mRequest->cookie[self::COOKIE_NAME_REQUESTID] = $id;
-		}
-		
-		return $id;
-	}
+    /**
+     *
+     * @var swoole_http_request
+     */
+    protected $mRequest = null;
 
-	public function get ($name) {
-		$post = $this->mRequest->post;
-		$get = $this->mRequest->get;
-		$cookie = $this->mRequest->cookie;
-		if (is_array($post) && array_key_exists($name, $post)) {
-			return $post[$name];
-		} else if (is_array($get) && array_key_exists($name, $get)) {
-			return $get[$name];
-		} else if (is_array($cookie) && array_key_exists($name, $cookie)) {
-			return $cookie[$name];
-		}
-		
-		return null;
-	}
+    /**
+     *
+     * @var array $mAllParam
+     */
+    protected $mAllParam = array();
 
-	public function getMethod () {
-		return $this->mRequest->server['request_method'];
-	}
+    public function __construct($req)
+    {
+        $this->mRequest = $req;
+    }
 
-	public function getURI () {
-		return $this->getRequestURI();
-	}
+    public function getId()
+    {
+        $id = $this->getCookie(self::COOKIE_NAME_REQUESTID);
+        if (empty($id)) {
+            $id = uuid_create();
+            
+            $this->mRequest->cookie[self::COOKIE_NAME_REQUESTID] = $id;
+        }
+        
+        return $id;
+    }
 
-	public function getRequestURI () {
-		$uri = urldecode($this->mRequest->server['request_uri']);
-		return $uri;
-	}
+    public function get($name)
+    {
+        $post = $this->mRequest->post;
+        $get = $this->mRequest->get;
+        $cookie = $this->mRequest->cookie;
+        $files = $this->files;
+        if (is_array($post) && array_key_exists($name, $post)) {
+            return $post[$name];
+        } else 
+            if (is_array($get) && array_key_exists($name, $get)) {
+                return $get[$name];
+            } else 
+                if (is_array($cookie) && array_key_exists($name, $cookie)) {
+                    return $cookie[$name];
+                } else 
+                    if (is_array($files) && array_key_exists($name, $files)) {
+                        return $files[$name];
+                    }
+        
+        return null;
+    }
 
-	public function isAjaxRequest () {
-		return ! empty($this->mRequest->header['http_x_requested_with']) &&
-				 strtolower($this->mRequest->header['http_x_requested_with']) == 'xmlhttprequest';
-	}
+    public function getMethod()
+    {
+        return $this->mRequest->server['request_method'];
+    }
 
-	public function getHeader ($fieldName) {
-		$fieldName = strtolower($fieldName);
-		return $this->mRequest->header[$fieldName];
-	}
+    public function getURI()
+    {
+        return $this->getRequestURI();
+    }
 
-	public function getClientIP () {
-		$IPaddress = '';
-		
-		if (isset($this->mRequest->header["http_x_forwarded_for"])) {
-			$IPaddress = $this->mRequest->header["http_x_forwarded_for"];
-		} else if (isset($this->mRequest->header["http_client_ip"])) {
-			$IPaddress = $this->mRequest->header["http_client_ip"];
-		} else if (isset($this->mRequest->server['remote_addr'])) {
-			$IPaddress = $this->mRequest->server["remote_addr"];
-		}
-		
-		return $IPaddress;
-	}
+    public function getRequestURI()
+    {
+        $uri = urldecode($this->mRequest->server['request_uri']);
+        return $uri;
+    }
 
-	public function getAllParams () {
-		if (empty($this->mAllParam)) {
-			if (is_array($this->mRequest->get)) {
-				$this->mAllParam = array_merge($this->mAllParam, $this->mRequest->get);
-			}
-			if (is_array($this->mRequest->post)) {
-				$this->mAllParam = array_merge($this->mAllParam, $this->mRequest->post);
-			}
-			if (is_array($this->mRequest->cookie)) {
-				$this->mAllParam = array_merge($this->mAllParam, $this->mRequest->cookie);
-			}
-		}
-		
-		return $this->mAllParam;
-	}
+    public function isAjaxRequest()
+    {
+        return ! empty($this->mRequest->header['http_x_requested_with']) && strtolower($this->mRequest->header['http_x_requested_with']) == 'xmlhttprequest';
+    }
 
-	public function getCookie ($name) {
-		$val = '';
-		if (is_array($this->mRequest->cookie)) {
-			$val = $this->mRequest->cookie[$name];
-		}
-		
-		return $val;
-	}
+    public function getHeader($fieldName)
+    {
+        $fieldName = strtolower($fieldName);
+        return $this->mRequest->header[$fieldName];
+    }
 
-	public function getAllCookie () {
-		$cookieArr = array();
-		if (is_array($this->mRequest->cookie)) {
-			$cookieArr = $this->mRequest->cookie;
-		}
-		
-		return $cookieArr;
-	}
+    public function getClientIP()
+    {
+        $IPaddress = '';
+        
+        if (isset($this->mRequest->header["http_x_forwarded_for"])) {
+            $IPaddress = $this->mRequest->header["http_x_forwarded_for"];
+        } else 
+            if (isset($this->mRequest->header["http_client_ip"])) {
+                $IPaddress = $this->mRequest->header["http_client_ip"];
+            } else 
+                if (isset($this->mRequest->server['remote_addr'])) {
+                    $IPaddress = $this->mRequest->server["remote_addr"];
+                }
+        
+        return $IPaddress;
+    }
 
-	public function pack () {
-		throw new NotImplementedException();
-	}
+    public function getAllParams()
+    {
+        if (empty($this->mAllParam)) {
+            if (is_array($this->mRequest->get)) {
+                $this->mAllParam = array_merge($this->mAllParam, $this->mRequest->get);
+            }
+            if (is_array($this->mRequest->post)) {
+                $this->mAllParam = array_merge($this->mAllParam, $this->mRequest->post);
+            }
+            if (is_array($this->mRequest->cookie)) {
+                $this->mAllParam = array_merge($this->mAllParam, $this->mRequest->cookie);
+            }
+        }
+        
+        return $this->mAllParam;
+    }
+
+    public function getCookie($name)
+    {
+        $val = '';
+        if (is_array($this->mRequest->cookie)) {
+            $val = $this->mRequest->cookie[$name];
+        }
+        
+        return $val;
+    }
+
+    public function getAllCookie()
+    {
+        $cookieArr = array();
+        if (is_array($this->mRequest->cookie)) {
+            $cookieArr = $this->mRequest->cookie;
+        }
+        
+        return $cookieArr;
+    }
+
+    public function pack()
+    {
+        throw new NotImplementedException();
+    }
 }
