c54ad9a8eb105ab0aaaa94a133a294e8d2968f49
Hoheart <youkejiang@che001.com>
2016-08-08 15:03:12 +0800

    test

--- IService.php
+++ IService.php
@@ -2,11 +2,12 @@
 namespace Framework;
 
 /**
- * 之所以设计这个，是为了解决个服务之间的先后依赖关系，所以，先启动的服务，必须后停止。
+ * 之所以test设计这个，是为了解决个服务之间的先后依赖关系，所以，先启动的服务，必须后停止。
  *
  * @author Hoheart
  */
-interface IService{
+interface IService
+{
 
     /**
      * 对服务进行初始化。比如：建立连接以用于发送请求数据包。
