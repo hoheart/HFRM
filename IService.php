d4898ce82ec60ce2697cc08ac0f9fb2acd3cff54
Hoheart <youkejiang@che001.com>
2016-08-08 11:28:04 +0800

    测试静态代码检查

--- IService.php
+++ IService.php
@@ -1,32 +1,31 @@
 <?php
-
 namespace Framework;
 
 /**
  * 之所以设计这个，是为了解决个服务之间的先后依赖关系，所以，先启动的服务，必须后停止。
  *
  * @author Hoheart
- *        
  */
-interface IService {
+interface IService
+{
 
-	/**
-	 * 对服务进行初始化。比如：建立连接以用于发送请求数据包。
-	 *
-	 * @param array $conf        	
-	 */
-	public function init (array $conf = array());
+    /**
+     * 对服务进行初始化。比如：建立连接以用于发送请求数据包。
+     *
+     * @param array $conf            
+     */
+    public function init(array $conf = array());
 
-	/**
-	 * 启动服务。
-	 */
-	public function start ();
+    /**
+     * 启动服务。
+     */
+    public function start();
 
-	/**
-	 * 停止服务，比如：提交事务，但不断开连接。
-	 *
-	 * @param bool $normal
-	 *        	是否正常结束，比如数据库对象，如果不是正常结束，应该回滚 。
-	 */
-	public function stop ($normal = true);
+    /**
+     * 停止服务，比如：提交事务，但不断开连接。
+     *
+     * @param bool $normal
+     *            是否正常结束，比如数据库对象，如果不是正常结束，应该回滚 。
+     */
+    public function stop($normal = true);
 }
